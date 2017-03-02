9341f6786b97227c624951d42b6803d98111697a
KorolD <98dima988@gmail.com>
2017-03-02 17:07:16 +0300

    Add files via upload

--- lab2/2.1.12.cpp
+++ lab2/2.1.12.cpp
@@ -5,8 +5,34 @@
 #include <tchar.h>
 #include <math.h>
 #include <iostream>
+#include <conio.h>
+
+bool check(double* s){
+	if(s[0]==0||s[1]==0||s[2]==0){
+	std::cout<<"Error.";
+	return 0;
+	}
+	return 1;
+}
+void fill(float* x, float* y, float* z, double* s){
+	std::cout<<"Type x1,y1,z1:";
+	std::cin>>x[0]>>y[0]>>z[0];
+	std::cout<<"Type x2,y2,z2:";
+	std::cin>>x[1]>>y[1]>>z[1];
+	std::cout<<"Type x3,y3,z3:";
+	std::cin>>x[2]>>y[2]>>z[2];
+	s[0]=sqrt(pow(x[0]-x[1],2)+pow(y[0]-y[1],2)+pow(z[0]-z[1],2));
+	s[1]=sqrt(pow(x[0]-x[2],2)+pow(y[0]-y[2],2)+pow(z[0]-z[2],2));
+	s[2]=sqrt(pow(x[2]-x[1],2)+pow(y[2]-y[1],2)+pow(z[2]-z[1],2));
+}
+
+void info(){
+	std::cout<<"\"Everything u wanted to know about triangles\". v 1.0.5 by Korolev Dima";
+	return;
+}
 
 void type(double s[3]){
+	std::cout<<"Your triangle is ";
 	if(s[0]==s[1]&&s[1]==s[2]){
 	std::cout<<"equilateral";
 	return;
@@ -53,47 +79,31 @@ int main()
 	std::cout<<"Type:\n1 to enter the coordinats;\n2 to get the type of your triangle;\n3 to get the perimeter;\n4 to get square;\n5 to get the radiuses;\n6 for info;\n7 to exit:  ";
 	std::cin>>n;
 	switch (n) {
-	case 1:{std::cout<<"Type x1,y1,z1:";
-			std::cin>>x[0]>>y[0]>>z[0];
-			std::cout<<"Type x2,y2,z2:";
-			std::cin>>x[1]>>y[1]>>z[1];
-			std::cout<<"Type x3,y3,z3:";
-			std::cin>>x[2]>>y[2]>>z[2];
-			s[0]=sqrt(pow(x[0]-x[1],2)+pow(y[0]-y[1],2)+pow(z[0]-z[1],2));
-			s[1]=sqrt(pow(x[0]-x[2],2)+pow(y[0]-y[2],2)+pow(z[0]-z[2],2));
-			s[2]=sqrt(pow(x[2]-x[1],2)+pow(y[2]-y[1],2)+pow(z[2]-z[1],2));
+	case 1:{fill(x,y,z,s);
 			break;
 		   }
-	case 2:if(s[0]==0||s[1]==0||s[2]==0){
-		   std::cout<<"Error.";
-		   break;
-		   }
-		   std::cout<<"Your triangle is ";
+	case 2:if(check(s)){
 		   type(s);
-		   break;
-	case 4:if(s[0]==0||s[1]==0||s[2]==0){
-		   std::cout<<"Error.";
-		   break;
 		   }
+		   break;
+	case 4:if(check(s)){
 		   std::cout<<"Square of your triangle is "<<square(s);
-		   break;
-	case 3:if(s[0]==0||s[1]==0||s[2]==0){
-		   std::cout<<"Error.";
-		   break;
 		   }
+		   break;
+	case 3:if(check(s)){
 		   std::cout<<"Perimeter of your triangle is "<<perimeter(s);
-		   break;
-	case 5:if(s[0]==0||s[1]==0||s[2]==0){
-		   std::cout<<"Error.";
-		   break;
 		   }
-		   circles(s);
 		   break;
-	case 6:std::cout<<"\"Everything u wanted to know about triangles\". v 1.0.5 by Korolev Dima";
+	case 5:if(check(s)){
+		   circles(s);
+		   }
+		   break;
+	case 6:info();
 		   break;
 	case 7:return 0;
 	default:std::cout<<"Error.";
 	}
-	std::cout<<"\n\n";
+	getch();
+	clrscr();
 	}
 }
